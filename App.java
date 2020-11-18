0865e259d90772bff9b04c9e5d88249ff95ab0c8
Diego-nrd <58235969+Diego-nrd@users.noreply.github.com>
2020-11-17 23:37:48 -0300

    Create App.java

---
+++ App.java
@@ -0,0 +1,45 @@
+/**
+ * @author Diego S da Silva
+ */
+
+
+package Robo;
+public class App {
+	public static void main(String [] args) {
+		Robo objRobo = new Robo();
+		Caixa objCaixa = new Caixa();
+		Mundo2D objSala = new Mundo2D();
+		for (int d = 0; d < 4; d++) {
+			for(int j = 1; j <= 10; j++) {
+				objRobo.printPos();
+				switch(d) {
+				 case 0 :
+					 objRobo.move(50 + j * 4, 50);
+					 
+					 break;
+				 
+				 case 1:
+					 objRobo.move(50, 50 + j * 4);
+					 
+					 break;
+				 
+				 case 2:
+					 objRobo.move(90 - j * 4, 90);
+					 
+					 break;
+					 
+				 case 3:
+					 objRobo.move(70, 50 - j * 4);
+				
+					 break;
+				}
+			}
+		}
+	
+		objRobo.printPos();
+		objRobo.printStatus();
+	  objCaixa.printStatus2();
+	  objSala.printStatus3();
+	}
+	
+}

61d2202178eb85d695a02551c08c86b0d65334e4
Diego-nrd <58235969+Diego-nrd@users.noreply.github.com>
2020-11-19 02:20:09 -0300

    Update caixaIdeia.java

--- caixaIdeia.java
+++ caixaIdeia.java
@@ -9,16 +9,12 @@ package Simulator.code;
 public class caixaIdeia {
 	
 
-protected double peso;
-protected float dimensaoC;
-protected float dimensaoL;
-protected float dimensaoA;
+  protected double peso;
+  protected float dimensaoC;
+  protected float dimensaoL;
+  protected float dimensaoA;
  	
-protected float posicaoX;
-protected float posicaoY;
+  protected float posicaoX;
+  protected float posicaoY;
 	
-	 
-
 }
-
-

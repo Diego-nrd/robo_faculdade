225009ddd8e17ea60910bc8dcd0d6170ae928894
Diego-nrd <58235969+Diego-nrd@users.noreply.github.com>
2020-11-19 02:11:09 -0300

    Create caixaIdeia.java

---
+++ caixaIdeia.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @Author Diego S da Silva
+ * 
+ */
+
+package Simulator.code;
+
+public class caixaIdeia {
+	
+
+protected double peso;
+protected float dimensaoC;
+protected float dimensaoL;
+protected float dimensaoA;
+ 	
+protected float posicaoX;
+protected float posicaoY;
+	
+	 
+
+}
+
+
